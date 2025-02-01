a5be7f7cd604327e6e10555c7f2c0ed92ec32363
pranshu <srivastavapranshu2@gmail.com>
2025-02-01 14:04:38 +0530

    added server instance in test file

--- test/faq.test.js
+++ test/faq.test.js
@@ -1,6 +1,6 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const server = require("../index");
+const app = require("../index");
 const FAQ = require("../models/FAQ");
 const { it } = require("mocha");
 chai.use(chaiHttp);
@@ -13,13 +13,14 @@ describe("FAQ API", () => {
 
 it("should create an faq", (done) => {
   chai
-    .request(server)
+    .request(app)
     .post("/api/faqs")
     .send({
       question: "What is Express?",
       answer: "Express is a web framework.",
     })
     .end((err, res) => {
+      if (err) done(err);
       expect(res.status).to.equal(201);
       done();
     });
@@ -27,9 +28,10 @@ it("should create an faq", (done) => {
 
 it("should fetch FAQs", (done) => {
   chai
-    .request(server)
+    .request(app)
     .get("/api/faqs")
     .end((err, res) => {
+      if (err) done(err);
       expect(res.status).to.equal(200);
       done();
     });
